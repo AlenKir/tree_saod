396adf48aece3d66ff4cdd224b8d5428dfe34f6a
AlenKir <kirillova.alena@gmail.com>
2016-11-19 12:25:54 +0300

    generates numbers for the tree...

--- main.cpp
+++ main.cpp
@@ -10,10 +10,13 @@ struct Tree {
 	Tree *left;
 	Tree *right;
 	int height;
-}
+};
 
 int main()
 {
+	Tree *root = new Tree;
+	bool isEmpty = true;
+
 	while (true) {
 	cout << "What do you want to do?" << endl;
 	cout << "1 - fill with random values" << endl;
@@ -28,6 +31,23 @@ int main()
 	if (x == 1)
 	{
 		//заполнить рандомно
+		int mas[N];
+		int i = 0;
+		while (i < N)
+		{
+			mas[i] = rand() % 10;
+			bool found = false;
+			for (int j = 0; j < N; j++)
+			{
+				if (i!=j && mas[i] == mas[j])
+				{ found = true; break;} 
+			}
+			if (!found)
+				i++;
+		}
+		for (int i = 0; i < N; i++)
+			cout << mas[i] << " " ;
+		cout << endl;
 	}
 	if (x == 2)
 	{
